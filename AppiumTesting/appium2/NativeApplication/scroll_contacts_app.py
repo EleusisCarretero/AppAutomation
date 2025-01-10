0e02822b2c076f3d5836c38b01409ebe71b60f89
Raul Eleusis Perez Carretero <eleusispcraul@gmail.com>
2025-01-10 14:56:12 -0600

    feat: test scroll down up contacts app

--- AppiumTesting/appium2/NativeApplication/scroll_contacts_app.py
+++ AppiumTesting/appium2/NativeApplication/scroll_contacts_app.py
@@ -18,9 +18,16 @@ if __name__ == "__main__":
     driver = webdriver.Remote('http://127.0.0.1:4723', options=options)
     # click on contacts button
     driver.find_element(AppiumBy.XPATH,'//android.widget.TextView[@content-desc="Contacts, Selected"]').click()
-    # Look for scrallable
+    # Scroll untill it founds an object with name "Betzabe"
     obj = driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR,'new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollIntoView(new UiSelector().textContains("Betzabe").instance(0))')
+    # Click on contact found
     obj.click()
+    # Scroll up and down
+    duration = 1000
+    for x, y, step_y in [(514, 600, -400), (514, 500, 300)]:
+        for _ in range(5):
+            driver.swipe(x,y,x,y + step_y, duration=duration)
+
 
     
 
